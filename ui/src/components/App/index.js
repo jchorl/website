d8d8318940c18e318746f8544d031665ab2c8b9c
Josh Chorlton <jchorlton@gmail.com>
2017-02-05 15:06:14 -0800

    skills bar is sticky and beautiful

--- ui/src/components/App/index.js
+++ ui/src/components/App/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Nav from '../Nav';
 import Home from '../Home';
 import Resume from '../Resume';
+import Projects from '../Projects';
 import './App.css';
 
 export default class App extends Component {
@@ -12,6 +13,7 @@ export default class App extends Component {
                 <Nav />
                 <Home />
                 <Resume />
+                <Projects />
             </div>
         );
     }
---
+++ ui/src/components/Projects/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Projects.css';
+
+export default class Projects extends Component {
+    render() {
+        return (
+            <div id="projects" className="noPrint">
+            </div>
+        );
+    }
+}
+
