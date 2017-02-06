e687e4f300e7b4d42c0c0215069a4f233ea31b72
Josh Chorlton <jchorlton@gmail.com>
2017-02-05 17:37:28 -0800

    projects scaffolding

--- ui/src/components/Projects/index.js
+++ ui/src/components/Projects/index.js
@@ -6,6 +6,12 @@ export default class Projects extends Component {
     render() {
         return (
             <div id="projects" className="noPrint">
+                <div id="projectsContent">
+                    <h1 className="sectionHeading">
+                        Projects
+                    </h1>
+                    <div>Click on the projects below to view the code on Github. All projects and more available at <a href="https://github.com/jchorl" target="_blank">github.com/jchorl</a>.</div>
+                </div>
             </div>
         );
     }
